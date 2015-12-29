9d74b46db3a973dfd574754512bfddb0f321b250
Victor <victor.vogel@hotmail.fr>
2015-12-29 12:28:34 +0100

    Création menu

--- src/Vue/Jeu.java
+++ src/Vue/Jeu.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Jeu extends Application {
@@ -19,20 +20,23 @@ public class Jeu extends Application {
     public static Scene scene;
     void sauvegarder(){}
     void charger(){}
-    void affichage(){}
 
     @Override
     public void start(Stage primaryStage) {
+        menus = new ArrayList<Menu>();
         root = new Group();
 
-        scene = new Scene(root, 1024, 768);
+        scene = new Scene(root, 800, 600);
 
         primaryStage.setTitle("Study Project Simulator");
         primaryStage.setScene(scene);
         //menus.add(new Menu());
-        ArbreDeCompetenceModele arbre = new ArbreDeCompetenceModele("info");
-        ArbreDeCompetenceVue arbreV = new ArbreDeCompetenceVue(arbre);
-        arbreV.affichage();
+        String[] boutons = new String[]{"Quitter", "Commencer"};
+        menus.add(new Menu(boutons));
+        menus.get(0).affichage();
+        //ArbreDeCompetenceModele arbre = new ArbreDeCompetenceModele("info");
+        //ArbreDeCompetenceVue arbreV = new ArbreDeCompetenceVue(arbre);
+        //arbreV.affichage();
         primaryStage.show();
     }
     public static void main(String[] args) { launch(args); }
--- src/Vue/Menu.java
+++ src/Vue/Menu.java
@@ -1,10 +1,54 @@
 package Vue;
 
-import java.util.List;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 
-/**
- * Created by Victor on 04/12/2015.
- */
+import java.util.ArrayList;
+import java.util.List;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+
 public class Menu {
-    void affichage(){}
+    final int positionY = 20;
+    final int espacement = 40;
+    final int largeur = 160;
+    final int hauteur = 40;
+    Group g;
+    List<Button> boutons;
+
+    Menu (String[] boutonTypes){
+        boutons = new ArrayList<Button>();
+        g = new Group();
+        int i = 1;
+        for(String type : boutonTypes) {
+            Button b = new Button(type);
+            b.setLayoutX((Jeu.scene.getWidth() - largeur) / 2);
+            b.setLayoutY((Jeu.scene.getHeight() / (boutonTypes.length * 2)) * i - hauteur / 2);
+            b.setMinSize(largeur, hauteur);
+            b.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    switch(type){
+                        case "Quitter":
+                            System.exit(0);
+                            break;
+                        case "Commencer":
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            });
+            boutons.add(b);
+            ++i;
+        }
+    }
+
+    void affichage(){
+        for(Button b : boutons) {
+            Jeu.root.getChildren().add(b);
+        }
+    }
 }
