272c1ad6c8e52efb5e6c5177c7b7980b0bd88af2
Anthony Gussy <anthonygussy@orange.fr>
2016-01-02 12:34:48 +0100

    Departement ajouté

--- src/Modele/Departement.java
+++ src/Modele/Departement.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Departement {
-    Compteur c;
     Enumerations.DepartementNom nom;
     int nbPersonne;
     List<Compteur> compteurs;
